a47bbb24f4559232845160e5efa623b59b3be728
EricPen <quzhen12@gmail.com>
2024-04-12 16:58:14 +0800

    fix

--- log/logger_test.go
+++ log/logger_test.go
@@ -5,8 +5,6 @@ import (
 )
 
 func TestDebugLog(t *testing.T) {
-	init2()
-	//Debug("test")
 	Info("test")
 	//logger.Info(fmt.Sprintf("%v", formatMsg("", 1, 2, "s", "s", errors.New("aa"), []string{"a", "b"})))
 }
