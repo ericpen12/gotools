4ec0cff6a06091e015f035ad860029616c977775
EricPen <quzhen12@gmail.com>
2024-04-15 17:20:10 +0800

    update file read

--- file/file.go
+++ file/file.go
@@ -4,9 +4,10 @@ import (
 	"bufio"
 	"io"
 	"os"
+	"path/filepath"
 )
 
-func ReadLine(filepath string, fn func(line string) bool) error {
+func ReadLine(filepath string, fn func(content string, line int) bool) error {
 	f, err := os.Open(filepath)
 	if err != nil {
 		return err
@@ -15,7 +16,8 @@ func ReadLine(filepath string, fn func(line string) bool) error {
 	return readLine(bufio.NewReader(f), fn)
 }
 
-func readLine(buf *bufio.Reader, fn func(line string) bool) error {
+func readLine(buf *bufio.Reader, fn func(content string, line int) bool) error {
+	var line int
 	for {
 		lineBytes, _, err := buf.ReadLine()
 		if err == io.EOF {
@@ -24,8 +26,26 @@ func readLine(buf *bufio.Reader, fn func(line string) bool) error {
 		if err != nil {
 			return err
 		}
-		if !fn(string(lineBytes)) {
+		line++
+		if !fn(string(lineBytes), line) {
 			return nil
 		}
 	}
+}
+
+func ReadLineByWalkDir(dir string, ignorePath []string, do func(content string, line int) bool) error {
+	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		for _, v := range ignorePath {
+			if path == v {
+				return nil
+			}
+		}
+		return ReadLine(path, do)
+	})
 }
